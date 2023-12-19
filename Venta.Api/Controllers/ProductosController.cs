81c89567795f3415264c22b5375d4ec14b8ced96
Javier Tunoque <javiertuga@gmail.com~>
2023-12-19 02:27:27 +0000

    Creando el proyecto WebApi e configurando  las capas de aplicacion e infraestructure dentro del proyecto

---
+++ Venta.Api/Controllers/ProductosController.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Venta.Application.CasosUso.AdministrarProductos.ConsultarProductos;
+
+namespace Venta.Api.Controllers
+{
+    public class ProductosController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ProductosController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<IActionResult> Consultar(ConsultarProductosRequest request)
+        {
+            var response = await _mediator.Send(request);
+
+            return Ok(response);
+        }
+    }
+}
