38ba5c6759fe261ba19fd1f709d56969296f7ec5
Javier Tunoque <javiertuga@gmail.com~>
2023-12-19 02:04:25 +0000

    adicionar la capa de infraestructura e implemetando los repositorios

---
+++ Venta.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Venta.Domain.Repositories;
+using Venta.Infrastructure.Repositories;
+
+namespace Venta.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static void AddInfraestructure(
+            this IServiceCollection services
+            )
+        {
+
+            services.AddRepositories();
+        }
+
+        private static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IProductoRepository, ProductoRepository>();
+
+            services.AddScoped<IVentaRepository, VentaRepository>();
+        }
+    }
+}
---
+++ Venta.Infrastructure/Repositories/VentaRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Venta.Domain.Repositories;
+
+namespace Venta.Infrastructure.Repositories
+{
+    public class VentaRepository : IVentaRepository
+    {
+        public Task<bool> Registrar(Domain.Models.Venta venta)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
