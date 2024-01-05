aa588307d6f3ead8861148f1479a103ae5177504
Jhon Pinedo Arbildo <jhonpinedo44@gmail.com>
2024-01-04 19:51:10 -0500

    Adicionando Cliente y Producto Configuration

--- Venta.Infrastructure/Repositories/Base/EFConfigurations/ClienteEntityTypeConfiguration.cs
+++ Venta.Infrastructure/Repositories/Base/EFConfigurations/ClienteEntityTypeConfiguration.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Venta.Domain.Models;
 
 namespace Venta.Infrastructure.Repositories.Base.EFConfigurations
 {
-    internal class ClienteEntityTypeConfiguration
-    {
-    }
+	public class ClienteEntityTypeConfiguration
+		: IEntityTypeConfiguration<Cliente>
+	{
+		public void Configure(EntityTypeBuilder<Cliente> builder)
+		{
+			builder.ToTable("Cliente");
+			builder.HasKey(c => c.IdCliente);
+		}
+	}
 }
