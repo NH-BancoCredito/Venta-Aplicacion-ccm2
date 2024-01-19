ea33c6151e26bc9e0de2d65b1e32a951874679d3
javier <javietuga@gmail.com>
2024-01-18 22:34:27 -0300

    Adicionar caso de uso de actualizar

---
+++ Venta.Application/CasosUso/AdministrarProductos/ActualizarProducto/ActualizarProductoHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Venta.Application.CasosUso.AdministrarProductos.ActualizarProducto
+{
+    public class ActualizarProductoHandler
+    {
+        /* 
+         1 - Deberia verificar si el productos
+         Si existe , entoces actualizar en la table de producto
+         Si no existe, crear un nuevo registro
+
+         */
+
+    }
+}
